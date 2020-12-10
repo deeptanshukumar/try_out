c220b73be592d26969df88d001cb38fd18272d5a
deeptanshukumar <70989817+deeptanshukumar@users.noreply.github.com>
2020-12-10 20:33:12 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fd2381a04e716001efc697c",
+				"5fd2381a04e716001efc697e"
+			]
+		},
+		"5fd2381a04e716001efc697c": {
+			"id": "5fd2381a04e716001efc697c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fd2381a04e716001efc697e": {
+			"id": "5fd2381a04e716001efc697e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
